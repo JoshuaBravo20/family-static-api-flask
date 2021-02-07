027056e334de13f2e8bda0d24b51f352f6536bdd
JoshuaBravo20 <69777229+JoshuaBravo20@users.noreply.github.com>
2021-02-07 20:44:26 -0300

    all-functional

--- src/app.py
+++ src/app.py
@@ -10,6 +10,7 @@ from datastructures import FamilyStructure
 
 app = Flask(__name__)
 app.url_map.strict_slashes = False
+app.config['ENV'] = 'development'
 CORS(app)
 
 # create the jackson family object
@@ -26,17 +27,75 @@ def sitemap():
     return generate_sitemap(app)
 
 @app.route('/members', methods=['GET'])
-def handle_hello():
+@app.route('/member', methods=['POST'])
+@app.route('/member/<int:id>', methods=['GET', 'DELETE', 'PUT'])
+def handle_hello(id = None):
 
-    # this is how you can use the Family datastructure by calling its methods
-    members = jackson_family.get_all_members()
-    response_body = {
-        "hello": "world",
-        "family": members
-    }
+    if request.method == 'GET':       
+        if id == None:
+            familia = jackson_family.get_all_members()
+            return jsonify(familia), 200
+        if id:
+            member = jackson_family.get_member(id)
+            return jsonify(member), 200
+
+    if request.method == 'POST':
+
+        id = request.json.get("id")
+        first_name = request.json.get("first_name")
+        last_name = "Jackson"
+        age = request.json.get("age")
+        lucky_numbers = request.json.get("lucky_numbers")
+
+        if not first_name:
+            return jsonify({"msg": "no first_name received, please type it in"}), 400
+        if not age:
+            return jsonify({"msg": "no age received, please type it in"}), 400
+        if not lucky_numbers:
+            return jsonify({"msg": "no lucky_numbers received, please type them in"}), 400
+
+        newMember = {
+            "first_name": first_name,
+            "age": age,
+            "lucky_numbers": lucky_numbers
+        }
+
+        jackson_family.add_member(newMember)
+        return jsonify({"success": "member has been created!"}), 200
+
+    if request.method == 'PUT':
+
+        member = jackson_family.get_member(id)
+
+        first_name = request.json.get("first_name")
+        last_name = "Jackson"
+        age = request.json.get("age")
+        lucky_numbers = request.json.get("lucky_numbers")
+
+        if not first_name:
+            return jsonify({"msg": "no first_name received, please type it in"}), 400
+        if not age:
+            return jsonify({"msg": "no age received, please type it in"}), 400
+        if not lucky_numbers:
+            return jsonify({"msg": "no lucky_numbers received, please type them in"}), 400
+
+        member["first_name"] = first_name
+        member["age"] = age
+        member["lucky_numbers"] = lucky_numbers
+
+        return jsonify({"success": "member has been edited!"}), 200
+
+    if request.method == 'DELETE':
+        member = jackson_family.get_member(id)
+        if not member: 
+            return jsonify({"error": "member does not exist!"}), 400
+        else:
+            jackson_family.delete_member(id)
+            return jsonify({"done": True}), 200
 
 
-    return jsonify(response_body), 200
+
+
 
 # this only runs if `$ python src/app.py` is executed
 if __name__ == '__main__':
